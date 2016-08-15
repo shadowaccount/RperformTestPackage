6f67ae958fda1cc705bb235a2b5be9af8601c249
analyticalmonk <akashtndn.acm@gmail.com>
2016-08-15 12:27:24 +0530

    Second Commit
    - Edited files R/memCheck.R and R/timeCheck.R
    - Function JustTimeIt in timeCheck contains a Sys.sleep(0.07) call and JustTimeIt2 contains a Sys.sleep(0.4) call
    - No tests created yet

--- R/memCheck.R
+++ R/memCheck.R
@@ -2,7 +2,8 @@ funWithMemory <- function() {
   # Nothing special
   data(BOD)
   BOD$Time <- 2 * BOD$Time
+  BOD <- rbind(BOD, BOD, BOD)
 #   > object.size(BOD)
-#   1088 bytes
+#   1280 bytes
   dim(BOD)
 }
--- R/timeCheck.R
+++ R/timeCheck.R
@@ -1,13 +1,13 @@
 justTimeIt <- function() {
   a <- 2 + 2
-  Sys.sleep(time = 0.05)
+  Sys.sleep(time = 0.07)
   b <- a + a
   b
 }
 
 justTimeIt2 <- function() {
   a <- 2 + 2
-  Sys.sleep(time = 0.2)
+  Sys.sleep(time = 0.4)
   b <- a + a
   b
 }
